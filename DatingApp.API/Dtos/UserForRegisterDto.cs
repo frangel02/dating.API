54f3b8e7b253b7dfa58a7999893eae6b368b3808
frodriguez <frangele331@gmail.com>
2020-03-05 00:40:26 -0400

    jwt authentication method login and dtos

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+       
+        [Required]
+        [StringLength(8, MinimumLength = 5, ErrorMessage="You must especify password between 4 and 8")]
         public string Password { get; set; }
 
     }
